198e5264660325760a6bb726977fcee36afd1538
Your Name <aditilahoria@gmail.com>
2017-01-09 16:24:18 +0530

    finaal2

--- api/db.php
+++ api/db.php
@@ -2,7 +2,7 @@
 function getDB() {
 	$dbhost="localhost";
 	$dbuser="root";
-	$dbpass="redhat@1111p";
+	$dbpass="redhat@11111p";
 	$dbname="time-board";
 	$dbConnection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
 	$dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
